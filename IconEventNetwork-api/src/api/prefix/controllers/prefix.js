0f0dc646b0ef2ee855ca3dcfbcd15f5c997ee079
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-14 18:32:31 -0400

    Add prefix collection type

---
+++ IconEventNetwork-api/src/api/prefix/controllers/prefix.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prefix controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::prefix.prefix');
