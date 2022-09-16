2267fe1a4a4568f82bb520c20f9c7b2515e7f28c
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-16 19:33:41 -0400

    Add AddressType collection

---
+++ IconEventNetwork-api/src/api/address-type/controllers/address-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * address-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::address-type.address-type');
---
+++ IconEventNetwork-api/src/api/address-type/services/address-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * address-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::address-type.address-type');
