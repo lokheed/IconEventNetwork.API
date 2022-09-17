c193cb15ce99e2235fefcf2aafdd9d3a658708d1
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-17 19:51:15 -0400

    Create PhoneNumberType collection

---
+++ IconEventNetwork-api/src/api/phone-number-type/routes/phone-number-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * phone-number-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::phone-number-type.phone-number-type');
