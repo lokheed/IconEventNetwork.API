8eb570cbcc62b29e4d6ddec4c7e35f9f429a3af8
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-17 20:18:36 -0400

    Create EmailAddressType collection

---
+++ IconEventNetwork-api/src/api/email-address-type/controllers/email-address-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * email-address-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::email-address-type.email-address-type');
---
+++ IconEventNetwork-api/src/api/email-address-type/services/email-address-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * email-address-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::email-address-type.email-address-type');
