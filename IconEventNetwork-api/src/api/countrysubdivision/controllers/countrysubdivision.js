8cb3b11f4184c6c1feb70fc7dd75e48e969dc6c4
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-28 09:53:49 -0400

    Create Country and CountrySubdivision collections

---
+++ IconEventNetwork-api/src/api/countrysubdivision/controllers/countrysubdivision.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * countrysubdivision controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::countrysubdivision.countrysubdivision');
---
+++ IconEventNetwork-api/src/api/countrysubdivision/routes/countrysubdivision.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * countrysubdivision router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::countrysubdivision.countrysubdivision');
