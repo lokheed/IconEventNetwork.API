8f7ff4c37ac8d9f160c64e6b8292fe9c41212b73
Ron Miles <rmiles@IconEventNetwork.com>
2022-09-29 12:27:33 -0400

    Add SocialMediaType collection

---
+++ IconEventNetwork-api/src/api/social-media-type/routes/social-media-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * social-media-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::social-media-type.social-media-type');
---
+++ IconEventNetwork-api/src/api/social-media-type/services/social-media-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * social-media-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::social-media-type.social-media-type');
