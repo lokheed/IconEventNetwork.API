599c032c45339b9e0127e37291716d6329532bcf
Ron Miles <rmiles@IconEventNetwork.com>
2022-10-04 20:54:21 -0400

    create footer menu components and single types

---
+++ IconEventNetwork-api/src/api/footer-legal-menu/controllers/footer-legal-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footer-legal-menu controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::footer-legal-menu.footer-legal-menu');
