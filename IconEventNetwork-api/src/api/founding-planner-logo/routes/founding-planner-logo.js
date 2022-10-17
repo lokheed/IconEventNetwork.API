ac98337c1221698137678f4114289e91cd441f53
Ron Miles <rmiles@IconEventNetwork.com>
2022-10-17 13:26:06 -0400

    Create FoundingPlannerLogos signle type

---
+++ IconEventNetwork-api/src/api/founding-planner-logo/routes/founding-planner-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * founding-planner-logo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::founding-planner-logo.founding-planner-logo');
