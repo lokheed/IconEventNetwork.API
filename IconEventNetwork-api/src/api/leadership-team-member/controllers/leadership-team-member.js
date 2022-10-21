66b991758991463d761a9525de7e09cbb9527114
Ron Miles <rmiles@IconEventNetwork.com>
2022-10-21 10:55:55 -0400

    Add LeadershipTeamMember collection

---
+++ IconEventNetwork-api/src/api/leadership-team-member/controllers/leadership-team-member.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * leadership-team-member controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::leadership-team-member.leadership-team-member');
---
+++ IconEventNetwork-api/src/api/leadership-team-member/services/leadership-team-member.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * leadership-team-member service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::leadership-team-member.leadership-team-member');
