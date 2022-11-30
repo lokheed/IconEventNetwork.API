6f4fa784014835f2bf714f6830c998561a8facb3
Ron Miles <rmiles@IconEventNetwork.com>
2022-11-29 19:10:36 -0500

    Update Strapi, create company status, type, subtype

---
+++ IconEventNetwork-api/src/api/company-status/routes/company-status.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * company-status router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::company-status.company-status');
---
+++ IconEventNetwork-api/src/api/company-sub-type/services/company-sub-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * company-sub-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::company-sub-type.company-sub-type');
