c779b09a024387ee2ca6d99ef0ffc10fdb90d566
Ron Miles <rmiles@IconEventNetwork.com>
2022-12-28 10:42:03 -0500

    Create and populate Language collection

--- IconEventNetwork-api/src/api/language/content-types/language/lifecycles.js
+++ IconEventNetwork-api/src/api/language/content-types/language/lifecycles.js
@@ -3,18 +3,18 @@ const tr = require('transliteration');
 module.exports = {
     
     async beforeCreate(event) {   
-        event.params.data.SearchableName = tr.transliterate(event.params.data.Name).toLowerCase();
-        event.params.data.A2 = event.params.data.A2.toUpperCase();
+        event.params.data.SearchableName = tr.transliterate(event.params.data.EnglishName).toLowerCase();
+        event.params.data.A2 = event.params.data.A2.toLowerCase();
         if (event.params.data.A2.length > 2) event.params.data.A2 = event.params.data.A2.substr(0,2);
-        event.params.data.A3 = event.params.data.A3.toUpperCase();
+        event.params.data.A3 = event.params.data.A3.toLowerCase();
         if (event.params.data.A3.length > 3) event.params.data.A3 = event.params.data.A3.substr(0,3);
      },
     
     async beforeUpdate(event) { 
-        event.params.data.SearchableName = tr.transliterate(event.params.data.Name).toLowerCase();
-        event.params.data.A2 = event.params.data.A2.toUpperCase();
+        event.params.data.SearchableName = tr.transliterate(event.params.data.EnglishName).toLowerCase();
+        event.params.data.A2 = event.params.data.A2.toLowerCase();
         if (event.params.data.A2.length > 2) event.params.data.A2 = event.params.data.A2.substr(0,2);
-        event.params.data.A3 = event.params.data.A3.toUpperCase();
+        event.params.data.A3 = event.params.data.A3.toLowerCase();
         if (event.params.data.A3.length > 3) event.params.data.A3 = event.params.data.A3.substr(0,3);
     },
   }
