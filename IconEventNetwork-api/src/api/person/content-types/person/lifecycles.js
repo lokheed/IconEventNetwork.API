b6384336b70e043ae57a5965e0a88c95762b624c
Ron Miles <rmiles@IconEventNetwork.com>
2023-01-06 11:27:56 -0500

    Task 443 - create fallback logic for new Person record

--- IconEventNetwork-api/src/api/person/content-types/person/lifecycles.js
+++ IconEventNetwork-api/src/api/person/content-types/person/lifecycles.js
@@ -1,41 +1,19 @@
-const tr = require('transliteration');
+const utils = require('@strapi/utils');
+const { ApplicationError } = utils.errors;
 
 module.exports = {
     
     async beforeCreate(event) {
-        // Trim up text fields
-        if (event.params.data.FirstName)  event.params.data.FirstName = event.params.data.FirstName.trim();
-        if (event.params.data.MiddleName)  event.params.data.MiddleName = event.params.data.MiddleName.trim();
-        if (event.params.data.LastName)  event.params.data.LastName = event.params.data.LastName.trim();
-        if (event.params.data.DirectoryName)  event.params.data.DirectoryName = event.params.data.DirectoryName.trim();
-        if (event.params.data.PreferredName)  event.params.data.PreferredName = event.params.data.PreferredName.trim();
-
-        // Default DirectoryName to "FirstName LastName" if DirectoryName is blank
-        if (!event.params.data.DirectoryName) event.params.data.DirectoryName = '';
-        if (event.params.data.DirectoryName === '' && event.params.data.FirstName && event.params.data.LastName) {
-            event.params.data.DirectoryName = event.params.data.FirstName + ' ' + event.params.data.LastName;
+        let userIds = event.params.data.Users.connect.map(u => u.id);
+        let hasExistingPersonRecord = await strapi.service('api::person.person').doesPersonExistForUser(userIds);
+        if (hasExistingPersonRecord) {
+            throw new ApplicationError('A Person object already exists for this User', {});
         }
-
-        // Set SearchableName to transliterated DirectoryName converted ot lower case
-        event.params.data.SearchableName = tr.transliterate(event.params.data.DirectoryName).toLowerCase();  
+        event = await strapi.service('api::person.person').cleanupFields(event);
     }, 
 
      async beforeUpdate(event) {   
-        // Trim up text fields
-        if (event.params.data.FirstName)  event.params.data.FirstName = event.params.data.FirstName.trim();
-        if (event.params.data.MiddleName)  event.params.data.MiddleName = event.params.data.MiddleName.trim();
-        if (event.params.data.LastName)  event.params.data.LastName = event.params.data.LastName.trim();
-        if (event.params.data.DirectoryName)  event.params.data.DirectoryName = event.params.data.DirectoryName.trim();
-        if (event.params.data.PreferredName)  event.params.data.PreferredName = event.params.data.PreferredName.trim();
-
-        // Default DirectoryName to "FirstName LastName" if DirectoryName is blank
-        if (!event.params.data.DirectoryName) event.params.data.DirectoryName = '';
-        if (event.params.data.DirectoryName === '' && event.params.data.FirstName && event.params.data.LastName) {
-            event.params.data.DirectoryName = event.params.data.FirstName + ' ' + event.params.data.LastName;
-        }
-
-        // Set SearchableName to transliterated DirectoryName converted ot lower case
-        event.params.data.SearchableName = tr.transliterate(event.params.data.DirectoryName).toLowerCase();   
+        event = await strapi.service('api::person.person').cleanupFields(event); 
     },
     
   }
--- IconEventNetwork-api/src/api/person/services/person.js
+++ IconEventNetwork-api/src/api/person/services/person.js
@@ -1,9 +1,44 @@
 'use strict';
-
+const tr = require('transliteration');
 /**
  * person service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::person.person');
+module.exports = createCoreService('api::person.person', ({ strapi }) =>  ({
+    async cleanupFields(event) {
+         // Trim up text fields
+         if (event.params.data.FirstName)  event.params.data.FirstName = event.params.data.FirstName.trim();
+         if (event.params.data.MiddleName)  event.params.data.MiddleName = event.params.data.MiddleName.trim();
+         if (event.params.data.LastName)  event.params.data.LastName = event.params.data.LastName.trim();
+         if (event.params.data.DirectoryName)  event.params.data.DirectoryName = event.params.data.DirectoryName.trim();
+         if (event.params.data.PreferredName)  event.params.data.PreferredName = event.params.data.PreferredName.trim();
+ 
+         // Default DirectoryName to "FirstName LastName" if DirectoryName is blank
+         if (!event.params.data.DirectoryName) event.params.data.DirectoryName = '';
+         if (event.params.data.DirectoryName === '' && event.params.data.FirstName && event.params.data.LastName) {
+             event.params.data.DirectoryName = event.params.data.FirstName + ' ' + event.params.data.LastName;
+         }
+ 
+         // Set SearchableName to transliterated DirectoryName converted ot lower case
+         event.params.data.SearchableName = tr.transliterate(event.params.data.DirectoryName).toLowerCase();  
+
+         return event;
+     },
+
+     async doesPersonExistForUser(userIds) {
+        const people = await strapi.entityService.findMany('api::person.person', {
+            populate: ['Users'],
+            filters: {
+                Users: {
+                    id: {
+                        $in: userIds
+                    }
+                }
+            },
+        });
+
+        return people.length > 0;
+     },
+}));
