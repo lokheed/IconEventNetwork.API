224325f7e6e29ad488f517bca80bdfbf384f65e7
Ron Miles <rmiles@IconEventNetwork.com>
2023-01-06 16:27:16 -0500

    Task 448 - refactor PhoneNumber lifecycle & tests

--- IconEventNetwork-api/src/api/phone-number/content-types/phone-number/lifecycles.js
+++ IconEventNetwork-api/src/api/phone-number/content-types/phone-number/lifecycles.js
@@ -1,53 +1,12 @@
-const fetch = require('node-fetch');
-
 module.exports = {
     
   async beforeCreate(event) {   
-    var countryId = event.params.data.country.connect[0] ? event.params.data.country.connect[0].id : 0;
-    if (countryId > 0) {
-      const countryCode = await strapi.service('api::country.country').getA2(countryId);
-      const verification = await strapi.service('api::phone-number.phone-number').verify(event.params.data.RawFormat, countryCode);
-
-      if (verification.isValid) {
-        event.params.data.IsValidated = true;
-        event.params.data.E164Format = verification.e164Format;
-        event.params.data.InternationalFormat = verification.internationalFormat;
-        event.params.data.NationalFormat = verification.nationalFormat;
-      }  
-    } else {
-      event.params.data.IsValidated = false;
-      event.params.data.E164Format = '';
-      event.params.data.InternationalFormat = '';
-      event.params.data.NationalFormat = '';
-    }
+    await strapi.service('api::phone-number.phone-number').validateInputs(event.params);
+    event = await strapi.service('api::phone-number.phone-number').setVerifiedInputs(event);
   },
   
   async beforeUpdate(event) { 
-    var countryId = event.params.data.country.connect[0] ? event.params.data.country.connect[0].id : 0;
-    if (countryId < 1) {
-      var phoneNumberId = event.params.where.id;
-      var existingPhone = await strapi.service('api::phone-number.phone-number')
-        .findOne(
-          phoneNumberId, 
-          {
-            fields: ['RawFormat'],
-            populate: { country: true },
-          });
-      countryId = existingPhone.country.id;
-    }
-    const countryCode = await strapi.service('api::country.country').getA2(countryId);
-    const verification = await strapi.service('api::phone-number.phone-number').verify(event.params.data.RawFormat, countryCode);
-      
-    if (verification.isValid) {
-      event.params.data.IsValidated = true;
-      event.params.data.E164Format = verification.e164Format;
-      event.params.data.InternationalFormat = verification.internationalFormat;
-      event.params.data.NationalFormat = verification.nationalFormat;
-    } else {
-      event.params.data.IsValidated = false;
-      event.params.data.E164Format = '';
-      event.params.data.InternationalFormat = '';
-      event.params.data.NationalFormat = '';
-    }
+    await strapi.service('api::phone-number.phone-number').validateInputs(event.params);
+    event = await strapi.service('api::phone-number.phone-number').setVerifiedInputs(event);
   },
 }
--- IconEventNetwork-api/src/api/phone-number/services/phone-number.js
+++ IconEventNetwork-api/src/api/phone-number/services/phone-number.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const utils = require('@strapi/utils');
+const { ApplicationError } = utils.errors;
 /**
  * phone-number service
  */
@@ -13,5 +14,66 @@ module.exports = createCoreService('api::phone-number.phone-number', ({ strapi }
         const response = await fetch(`https://api.bigdatacloud.net/data/phone-number-validate?number=${encodedPhoneNumber}&countryCode=${countryCode}&key=${process.env.BIG_DATA_CLOUD_KEY}`);
         const verification = await response.json();
         return verification;
-    }
+    },
+
+    async validateInputs(params) {
+        let countryId = params.data.country.connect[0] ? params.data.country.connect[0].id : 0;
+        let phoneTypeId = params.data.phone_number_type.connect[0] ? params.data.phone_number_type.connect[0].id : 0;
+        const phoneNumberId = params.where?.id ? params.where.id : 0;
+        if ((countryId === 0 || phoneTypeId === 0) && phoneNumberId > 0) {
+            const existingPhone = await strapi.service('api::phone-number.phone-number')
+                .findOne(
+                phoneNumberId, 
+                {
+                    fields: ['RawFormat'],
+                    populate: { country: true, phone_number_type: true },
+                });
+            countryId = countryId > 0 ? countryId : existingPhone.country.id;
+            phoneTypeId = phoneTypeId > 0 ? phoneTypeId : existingPhone.phone_number_type.id;
+        }
+
+        if (countryId === 0) throw new ApplicationError('Country is a required field.', {});        
+        if (phoneTypeId === 0) throw new ApplicationError('PhoneNumberType is a required field.', {});
+    },
+
+    async setVerifiedInputs(event) {
+        let countryId = event.params.data.country.connect[0] ? event.params.data.country.connect[0].id : 0;
+        const phoneNumberId = event.params.where?.id ? event.params.where.id : 0;
+        if (countryId === 0 && phoneNumberId > 0) {
+          const existingPhone = await strapi.service('api::phone-number.phone-number')
+            .findOne(
+              phoneNumberId, 
+              {
+                fields: ['RawFormat'],
+                populate: { country: true },
+              });
+          countryId = existingPhone.country.id;
+        }
+        if (countryId > 0) {
+            const countryCode = await strapi.service('api::country.country').getA2(countryId);
+            const verification = await strapi.service('api::phone-number.phone-number').verify(event.params.data.RawFormat, countryCode);    
+            
+            if (verification.isValid) {
+                event.params.data.IsValidated = true;
+                event.params.data.E164Format = verification.e164Format;
+                event.params.data.InternationalFormat = verification.internationalFormat;
+                event.params.data.NationalFormat = verification.nationalFormat;
+            } else {
+                event.params.data.IsValidated = false;
+                event.params.data.E164Format = '';
+                event.params.data.InternationalFormat = '';
+                event.params.data.NationalFormat = '';
+            }
+            return event;
+        }
+          
+        // no valid country was passed in, reset verified inputs
+        event.params.data.IsValidated = false;
+        event.params.data.E164Format = '';
+        event.params.data.InternationalFormat = '';
+        event.params.data.NationalFormat = '';
+
+        return event;
+    },
+
 }));
