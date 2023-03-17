9bd322a6ab4e98f62bff61ef3ab337085c444155
Ron Miles <rmiles@IconEventNetwork.com>
2023-03-17 13:25:50 -0400

    Fix bug with company name being removed

--- IconEventNetwork-api/src/api/company/services/company.js
+++ IconEventNetwork-api/src/api/company/services/company.js
@@ -10,7 +10,6 @@ const { createCoreService } = require('@strapi/strapi').factories;
 module.exports = createCoreService('api::company.company', ({ strapi }) =>  ({
     async cleanupFields(event) {
          // Trim up text fields
-         if (!event.params.data.Name) event.params.data.Name = '';
          if (event.params.data.Name)  event.params.data.Name = event.params.data.Name.trim();
          if (event.params.data.InvoiceCompanyName)  event.params.data.InvoiceCompanyName = event.params.data.InvoiceCompanyName.trim();
          if (event.params.data.Tagline)  event.params.data.Tagline = event.params.data.Tagline.trim();
@@ -19,12 +18,12 @@ module.exports = createCoreService('api::company.company', ({ strapi }) =>  ({
  
          // Default InvoiceCompanyName to "CompanyName" if InvoiceCompanyName is blank
          if (!event.params.data.InvoiceCompanyName) event.params.data.InvoiceCompanyName = '';
-         if (event.params.data.InvoiceCompanyName === '' && event.params.data.Name) {
+         if ((event.params.data.InvoiceCompanyName??'') === '' && event.params.data.Name) {
              event.params.data.InvoiceCompanyName = event.params.data.Name;
          }
  
          // Set SearchableName to transliterated Name converted ot lower case
-         event.params.data.SearchableName = tr.transliterate(event.params.data.Name).toLowerCase();  
+         if (event.params.data.Name)  event.params.data.SearchableName = tr.transliterate(event.params.data.Name).toLowerCase();  
 
          return event;
      },
