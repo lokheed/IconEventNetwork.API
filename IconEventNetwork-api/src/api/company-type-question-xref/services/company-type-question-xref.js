4a91c1d982925b03ec8d6abe7d8a2fea7c6a7e76
Ron Miles <rmiles@IconEventNetwork.com>
2023-04-19 09:14:13 -0400

    US473 T533 question and answer collections

---
+++ IconEventNetwork-api/src/api/company-type-question-xref/services/company-type-question-xref.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * company-type-question-xref service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::company-type-question-xref.company-type-question-xref');
